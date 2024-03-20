2f5aa926c7b4093a3d122792676140cef8df67fe
Thiago Borda <thiagoborda7@gmail.com>
2024-03-20 18:31:35 -0300

    second commit

--- archivos.-js/script.js
+++ archivos.-js/script.js
@@ -7,3 +7,24 @@ icono.addEventListener('click', () => {
     icono.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 })
+
+
+const menuLinks = document.querySelectorAll('.navbar a[href^="#"]');
+
+menuLinks.forEach(links => {
+    links.addEventListener('click', function() {
+        navbar.classList.remove('active');
+        icono.classList.remove('bx-x');
+    })
+})
+
+//ACTIVE LINKS NAVBAR
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+function activeLink() {
+    navLinks.forEach(b => b.classList.remove('active'));
+    this.classList.add('active');
+}
+
+navLinks.forEach(b => b.addEventListener('click', activeLink));
